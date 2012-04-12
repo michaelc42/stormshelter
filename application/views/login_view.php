89a3d77e758a2e5f880945986c86a3f644fbef4e
Michael Casey <michaelcasey42@gmail.com>
2012-04-11 19:20:19 -0500

    Working on admin-template

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,4 +1,32 @@
 <?php if (!($this->session->userdata('logged_in'))): ?>
+
+<div id="container" class="grid_16 alpha omega">
+	
+
+
+<div class="grid_16 alpha" id="admin-header">
+	<a class= "grid_4 alpha" id="logo-link" href="http://www.f5stormrooms.com/">
+		<img id="main-logo" class="" src="<?php echo base_url();?>images/logo3.png" alt="F5 Stormrooms Logo"/>
+	</a>
+	
+	<div class="grid_12 alpha omega" id="admin-menu">
+		<a class="grid_2 alpha" href="galleries">
+			Galleries
+		</a>
+		<a class="grid_2 alpha" href="addGallery">
+			New Gallery
+		</a>
+		
+		<a class="grid_2 alpha" href="addphoto">
+			Add Photo
+		</a>
+		
+		<a class="grid_2 alpha suffix_4" href="logout">
+			Logout
+		</a>
+	</div>
+</div>
+
 <?php 
 	$data = array(
 	  'name'        => 'username',
@@ -18,14 +46,14 @@
 		'style'       => 'width:100px',
 	);
 ?>
-<div class="grid_16" id="">			
+<div class="grid_16 alpha" id="login-form">			
 	<?php echo form_open('user/login'); //add the path ?>
 	<?php echo form_input($data); ?>
 	<?php echo form_password($data2); ?>
 	<?php echo form_submit('submit', 'Login'); ?>
 	<?php echo form_close(); ?>
 </div>
-<div class="grid_16 errors">
+<div class="grid_16 errors alpha">
 	<p><?php echo $error = (!empty($errors)) ? $errors : ''; ?></p>
 </div>
 <?php endif; ?>
