8c1c5c60fc5b7f1196ee8c65c635ba7741004484
Michael Casey <michaelcasey42@gmail.com>
2012-05-02 20:36:27 -0500

    Fixed admin_galleries_view -Max height and width

--- application/views/user_galleries_view.php
+++ application/views/user_galleries_view.php
@@ -5,24 +5,27 @@
 <div class="all_galleries grid_16 alpha omega">
 <?php foreach ($galleries as $gallery) : ?>
 	<div class="gallery grid_4 alpha">
-		<h3><?php echo $gallery->title; ?></h3>
+		<h3><?php echo $gallery->title; ?></h3>			
+		<a href="<?php echo site_url().'user/gallery/'.$gallery->id; ?>">
 		<p>
-			<a href="<?php echo site_url().'user/gallery/'.$gallery->id; ?>">
-				<?php if ( $gallery->front_image ) : ?>
-					<img src="<?php echo site_url().'uploads/'.$gallery->directory_name.'/thumbs/'.$gallery->front_image;?>" alt="image"/>
-				<?php else: ?>					
-					<img src="<?php echo site_url().'uploads/default_image.gif'; ?>" alt="image"/>
-				<?php endif; ?>
-			</a>
-		</p>
+			<div class="image"
+			style="background-image: url(
+			<?php if ( $gallery->front_image ) : ?>
+				<?php echo site_url();?>uploads/<?php echo $gallery->directory_name;?>/thumbs/<?php echo $gallery->front_image;?>
+			<?php else: ?>					
+				<?php echo site_url();?>uploads/default_image.gif 
+			<?php endif; ?>
+			)">
+			</div>
+			</p>
+		</a>
 		<p>
-			<?php echo $gallery->description; ?>	
-		</p>
-		
-		<p>
-			
 			<a href="<?php echo site_url().'user/addphoto/'.$gallery->directory_name; ?>">Add Photo</a>
 			<a href="<?php echo site_url(); ?>user/confirmDelete/<?php echo $gallery->id; ?>">delete</a>
+		</p>
+		
+		<p class="desc">
+			<?php echo $gallery->description; ?>	
 		</p>
 	</div> 
 <?php endforeach; ?>
